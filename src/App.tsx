c805ba52c14443ea61a14856ddc54b3d62c0ebaf
aarstanbekov <aarstanbekov@innoforce.kz>
2024-10-26 11:19:26 +0500

    feat: added prettier and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 export function App() {
   return (
-    <div className="App">
-
+    <div>
+      <h1 className="text-green-400">adasd</h1>
     </div>
   );
 }
