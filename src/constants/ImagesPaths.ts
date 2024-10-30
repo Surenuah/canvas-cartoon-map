14a7bb86936aa86b072d6d298dc417ed84b1336d
aarstanbekov <aarstanbekov@innoforce.kz>
2024-10-30 20:45:41 +0500

    feat: added map image

--- src/constants/ImagesPaths.ts
+++ src/constants/ImagesPaths.ts
@@ -1,6 +1,11 @@
 export const Images = {
-	HOME: 'assets/Home.png',
-	WIZ: 'assets/Lightning.png',
-	EDUCATION: 'assets/AcademicCap.png',
-	PROFILE: 'assets/User.png',
+  HOME: "assets/Home.png",
+  WIZ: "assets/Lightning.png",
+  EDUCATION: "assets/AcademicCap.png",
+  PROFILE: "assets/User.png",
 };
+
+export const MapImg = "assets/map.png";
+export const PlayerImg = "monkey.png";
+export const SecondPosition = "secondPosition.png";
+export const ThirdPosition = "thirdPosition.png";
--- src/pages/EducationPage.tsx
+++ src/pages/EducationPage.tsx
@@ -1,3 +1,17 @@
+import { Layer, Stage } from "react-konva";
+import { CustomCanvasImage } from "../components/CustomCanvasImage";
+import { MapImg } from "../constants/ImagesPaths";
+
 export const EducationPage = () => {
-	return <>Education</>;
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <CustomCanvasImage
+          src={MapImg}
+          width={window.innerWidth}
+          height={window.innerHeight}
+        />
+      </Layer>
+    </Stage>
+  );
 };
