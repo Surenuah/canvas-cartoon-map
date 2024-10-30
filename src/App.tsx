acf55c9cf3da740f90d8c9b7a07d1253d1b6427b
aarstanbekov <aarstanbekov@innoforce.kz>
2024-10-30 14:08:13 +0500

    feat: added HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import "./App.css";
-import { Outlet } from "react-router-dom";
+import './App.css';
+import { Outlet } from 'react-router-dom';
 
 export const App = () => {
-  return (
-    <>
-      <h1>Мапа</h1>
-      <main>
-        <Outlet />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<main>
+				<Outlet />
+			</main>
+		</>
+	);
 };
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,51 +1,50 @@
-import { createBrowserRouter, Link } from "react-router-dom";
-import { App } from "../App";
+import { createBrowserRouter, Link } from 'react-router-dom';
+import { App } from '../App';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: (
-          <div>
-            <h1>Главная</h1>
-            <Link to="wiz">Перейти на WIZ</Link>
-            <Link to="ed">Перейти на ED</Link>
-            <Link to="profile">Перейти на Профиль</Link>
-          </div>
-        ),
-      },
-      {
-        path: "wiz",
-        element: (
-          <div>
-            <h1>WIZ</h1>
-            <Link to="/">Вернуться на Главную</Link>
-          </div>
-        ),
-      },
-      {
-        path: "ed",
-        element: (
-          <div>
-            <h1>ED</h1>
-            <Link to="/">Вернуться на Главную</Link>
-          </div>
-        ),
-      },
-      {
-        path: "profile",
-        element: (
-          <div>
-            <h1>Профиль</h1>
-            <Link to="/">Вернуться на Главную</Link>
-          </div>
-        ),
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: (
+					<div className='bg-gray-200 rounded-md'>
+						<Link to='wiz'>Перейти на WIZ</Link>
+						<Link to='ed'>Перейти на ED</Link>
+						<Link to='profile'>Перейти на Профиль</Link>
+					</div>
+				),
+			},
+			{
+				path: 'wiz',
+				element: (
+					<div>
+						<h1>WIZ</h1>
+						<Link to='/'>Вернуться на Главную</Link>
+					</div>
+				),
+			},
+			{
+				path: 'ed',
+				element: (
+					<div>
+						<h1>ED</h1>
+						<Link to='/'>Вернуться на Главную</Link>
+					</div>
+				),
+			},
+			{
+				path: 'profile',
+				element: (
+					<div>
+						<h1>Профиль</h1>
+						<Link to='/'>Вернуться на Главную</Link>
+					</div>
+				),
+			},
+		],
+	},
 ]);
 
 export default router;
